e27e7facf33dd2cd321f3a05b108dc6a4efcee94
Pedro José Romero León <63571272+Peter24K2G@users.noreply.github.com>
2025-03-20 09:01:11 -0500

    Update Practica.py

--- pages/Practica.py
+++ pages/Practica.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
+import numpy as np
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
