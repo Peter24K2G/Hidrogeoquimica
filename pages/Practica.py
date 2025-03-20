6b1816e7de3fd65e30bde5347ba8752f63f9cdc9
Pedro José Romero León <63571272+Peter24K2G@users.noreply.github.com>
2025-03-20 11:44:22 -0500

    Update Practica.py

--- pages/Practica.py
+++ pages/Practica.py
@@ -272,6 +272,7 @@ if df is not None:  # Solo ejecutar si hay datos cargados correctamente
             # df = pd.read_csv('Insumos/PlantillaFQO.csv')
             Sample = st.selectbox("Selecciona la muestra a anlizar:", df["Sample"].unique())
             ndf = df.loc[df["Sample"] == Sample]
+            st.write(ndf)
             stiff.plot(ndf, unit='mg/L', figname='images/Stiff diagram', figformat='jpg')
             files = [x for x in os.listdir("images") if x.startswith("Stiff")]
             imageselection = st.selectbox("Imagen ejemplo a visualizar:",
