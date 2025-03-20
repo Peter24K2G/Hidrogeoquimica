40c94e7fcc7cbb038889bdd715a666dabfa0f2e8
Pedro José Romero León <63571272+Peter24K2G@users.noreply.github.com>
2025-03-20 09:06:28 -0500

    Update Practica.py

--- pages/Practica.py
+++ pages/Practica.py
@@ -137,7 +137,7 @@ if df is not None:  # Solo ejecutar si hay datos cargados correctamente
             st.image("images/Schoeller_diagram.jpg", caption="Diagrama de Schoeller", use_container_width=True)
 
     elif st.session_state.page == 7:
-            st.markdown("""
+        st.markdown("""
         # ANÁLISIS ESTADÍSTICO MULTIVARIADO
         Se presentan algunas rutinas básicas para el Análisis de Componentes Principales y de Agrupamiento (Cluster) utilizando resultados de análisis fisicoquímicos de agua subterránea (CAMPUS UNAL 2023). Las concentraciones se reportan en meq/L.
         Para la preparación de la base de datos, se generan algunos estadísticos y gráficos descriptivos, evaluación de la normalidad de los parámetros y estandarización de los datos.
